d42918aac4a66865f571e7ccd8d6c29a35c3137a
ALISHBA MUSHARRAF <162012518+alishbamusharraf@users.noreply.github.com>
2024-11-27 22:12:30 +0500

    Your commit message

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../components/footer'
+import Skills from '../components/skills'
+const page = () => {
+    return (
+      <div>
+    <Footer/>
+    <Skills/>
+     </div>
+    )
+  }
+
+export default page
